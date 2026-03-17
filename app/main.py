109a7d70fbf31338b25b5eb5eb099b7481242708
Anton Gaievyi <anton.gaievyi@gmail.com>
2026-03-17 20:11:04 +0100

    redesigned py-car-wah-station

--- app/main.py
+++ app/main.py
@@ -15,17 +15,17 @@ class CarWashStation:
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
-            if car.clean_mark < self.clean_power:
-                income += self.wash_single_car(car)
-                car.clean_mark = self.clean_power
-        return income
+            income += self.wash_single_car(car)
+        return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> float:
-        price = self.calculate_washing_price(car)
-        car.clean_mark = self.clean_power
+        price = 0
+        if car.clean_mark < self.clean_power:
+            price = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
         return price
 
     def rate_service(self, rate: int) -> None:
